93be27c38f1c4c40a3917c390793ac19ea991f5b
Bruno Timeus <timeusbruno@gmail.com>
2025-06-18 10:21:55 +0200

    Crea endpoint index

---
+++ src/main/java/com/example/demo/web/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.demo.web;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+    
+    @GetMapping("")
+    public ModelAndView Index() {
+        return new ModelAndView("index");
+    }
+}
